af30f364bf05f05f07e95b22bad186bf4cd4ff46
divinmik <mikdivin@gmail.com>
2025-12-22 11:10:13 +0100

    none dans transfert

--- api/views/resources.py
+++ api/views/resources.py
@@ -546,7 +546,8 @@ class DemandeViewSet(AuditModelViewSet):
         departement_source = last_transfer.departement_beneficiaire if last_transfer else demande.id_departement
         with transaction.atomic():
             demande.id_departement = departement
-            demande.save(update_fields=['id_departement'])
+            demande.agent_traitant = ""
+            demande.save(update_fields=['id_departement', 'agent_traitant'])
             Transfert.objects.create(
                 departement_source=departement_source,
                 departement_beneficiaire=departement,
